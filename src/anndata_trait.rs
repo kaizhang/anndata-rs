bf0a9ed1e731940e8483efd535fd37aa8ff1760e
Kai Zhang <kai@kzhang.org>
2022-03-10 10:10:51 -0800

    update

--- src/anndata_trait.rs
+++ src/anndata_trait.rs
@@ -1,15 +1,16 @@
 use crate::utils::{create_str_attr, read_str_attr, COMPRESSION};
 
-use ndarray::{arr1, Array1, Array, Dimension};
-use hdf5::{
-    File, H5Type, Result, Group, Dataset,
-    types::{VarLenUnicode, TypeDescriptor},
+use itertools::zip;
+use ndarray::{Axis, Array1, Array2, Array, Dimension};
+use hdf5::{H5Type, Result, Group, Dataset,
+    types::TypeDescriptor,
 };
-use sprs::CsMat;
+use nalgebra_sparse::csr::CsrMatrix;
 
 #[derive(Debug, PartialEq)]
 pub enum DataType {
-    CsMatrix(TypeDescriptor),
+    CsrMatrix(TypeDescriptor),
+    CscMatrix(TypeDescriptor),
     Vector(TypeDescriptor),
     Array(TypeDescriptor),
     StringVector,
@@ -40,12 +41,22 @@ impl ContainerType for Dataset {
 
 impl dyn ContainerType {
     pub fn get_encoding_type(&self) -> Result<DataType> {
-        match self._encoding_type()?.as_str() {
-            "csr_matrix" | "csc_matrix" => {
+        match self._encoding_type().unwrap_or("array".to_string()).as_ref() {
+            "array" => {
+                let dataset: &Dataset = self.as_ref();
+                let ty = dataset.dtype()?.to_descriptor()?;
+                Ok(DataType::Array(ty))
+            }
+            "csr_matrix" => {
                 let group: &Group = self.as_ref();
                 let ty = group.dataset("data")?.dtype()?.to_descriptor()?;
-                Ok(DataType::CsMatrix(ty))
+                Ok(DataType::CsrMatrix(ty))
             },
+            "csc_matrix" => {
+                let group: &Group = self.as_ref();
+                let ty = group.dataset("data")?.dtype()?.to_descriptor()?;
+                Ok(DataType::CscMatrix(ty))
+            }
             _ => todo!()
         }
     }
@@ -92,29 +103,28 @@ pub trait AnnDataType {
     fn dtype() -> DataType where Self: Sized;
 }
 
-impl<T> AnnDataType for CsMat<T>
+impl<T> AnnDataType for CsrMatrix<T>
 where
     T: H5Type,
 {
     fn write(&self, location: &Group, name: &str) -> Result<Box<dyn ContainerType>>
     {
         let group = location.create_group(name)?;
-        let encoding = if self.is_csr() { "csr_matrix" } else { "csc_matrix" };
-        create_str_attr(&group, "encoding-type", encoding)?;
+        create_str_attr(&group, "encoding-type", "csr_matrix")?;
         create_str_attr(&group, "encoding-version", self.version())?;
 
         group.new_attr_builder()
-            .with_data(&[self.rows(), self.cols()]).create("shape")?;
+            .with_data(&[self.nrows(), self.ncols()]).create("shape")?;
         group.new_dataset_builder().deflate(COMPRESSION)
-            .with_data(self.data()).create("data")?;
+            .with_data(self.values()).create("data")?;
 
         // TODO: fix index type
-        let indices: Array1<i32> = self.indices().iter()
+        let indices: Array1<i32> = self.col_indices().iter()
             .map(|x| *x as i32).collect(); // scipy compatibility
         group.new_dataset_builder().deflate(COMPRESSION)
             .with_data(&indices).create("indices")?;
 
-        let indptr: Array1<i32> = self.proper_indptr().into_owned().iter()
+        let indptr: Array1<i32> = self.row_offsets().iter()
             .map(|x| *x as i32).collect();  // scipy compatibility
         group.new_dataset_builder().deflate(COMPRESSION)
             .with_data(&indptr).create("indptr")?;
@@ -130,34 +140,48 @@ where
         let indptr: Vec<usize> = dataset.dataset("indptr")?.read_raw()?;
 
         match dataset._encoding_type()?.as_str() {
-            "csr_matrix" => Ok(CsMat::new(
-                (shape[0], shape[1]),
+            "csr_matrix" => Ok(CsrMatrix::try_from_csr_data(
+                shape[0],
+                shape[1],
                 indptr,
                 indices,
                 data,
-            )),
-            "csc_matrix" => Ok(CsMat::new_csc(
-                (shape[0], shape[1]),
-                indptr,
-                indices,
-                data,
-            )),
-            _ => Err(hdf5::Error::from("not a sparse matrix!")),
+            ).unwrap()),
+            _ => Err(hdf5::Error::from("not a csr matrix!")),
         }
     }
 
-    fn get_dtype(&self) -> DataType {
-        DataType::CsMatrix(T::type_descriptor())
-    }
-
-    fn dtype() -> DataType {
-        DataType::CsMatrix(T::type_descriptor())
-    }
-
-    fn version(&self) -> &str {
-        "0.1.0"
-    }
+    fn get_dtype(&self) -> DataType { DataType::CsrMatrix(T::type_descriptor()) }
+    fn dtype() -> DataType { DataType::CsrMatrix(T::type_descriptor()) }
+    fn version(&self) -> &str { "0.1.0" }
 }
+
+impl<T, D> AnnDataType for Array<T, D>
+where
+    T: H5Type,
+    D: Dimension,
+{
+    fn write(&self, location: &Group, name: &str) -> Result<Box<dyn ContainerType>>
+    {
+        let dataset = location.new_dataset_builder().deflate(COMPRESSION)
+            .with_data(self).create(name)?;
+
+        create_str_attr(&*dataset, "encoding-type", "array")?;
+        create_str_attr(&*dataset, "encoding-version", self.version())?;
+        Ok(Box::new(dataset))
+    }
+
+    fn read(container: &Box<dyn ContainerType>) -> Result<Self> where Self: Sized {
+        let dataset: &Dataset = container.as_ref().as_ref();
+        dataset.read()
+    }
+
+    fn get_dtype(&self) -> DataType { DataType::Array(T::type_descriptor()) }
+    fn dtype() -> DataType { DataType::Array(T::type_descriptor()) }
+    fn version(&self) -> &str { "0.2.0" }
+}
+
+
 
 
 
@@ -189,34 +213,6 @@ impl<D: H5Type + Clone> AnnDataType for Vec<D> {
     }
 
     fn version(&self) -> &str { "0.1.0" }
-}
-
-impl<A, D> AnnDataType for Array<A, D>
-where
-    A: H5Type,
-    D: Dimension,
-{
-    type Container = Dataset;
-
-    fn write(&self, location: &Group, name: &str) -> Result<Self::Container>
-    {
-        let dataset = location.new_dataset_builder().deflate(COMPRESSION)
-            .with_data(self).create(name)?;
-
-        create_str_attr(&*dataset, "encoding-type", "array")?;
-        create_str_attr(&*dataset, "encoding-version", self.version())?;
-
-        Ok(dataset)
-    }
-
-    fn read(dataset: &Self::Container) -> Result<Self> { dataset.read() }
-
-    fn update(&self, container: &Self::Container) -> Result<()> {
-        todo!()
-    }
-
-    fn dtype(&self) -> DataType { DataType::Array(A::type_descriptor()) }
-    fn version(&self) -> &str { "0.2.0" }
 }
 
 impl AnnDataType for StrVec {
@@ -268,31 +264,115 @@ impl AsRef<CsrMatrix<T>> for dyn AnnDataType {
 }
 */
 
-pub trait AnnDataView: AnnDataType {
-    fn read_partial(
+pub trait AnnDataGetRow: AnnDataType {
+    fn read_rows(
         container: &Box<dyn ContainerType>,
-        ridx: Vec<usize>,
-        cidx: Vec<usize>,
-    ) -> Result<Self> where Self: Sized;
+        idx: Option<&[usize]>,
+    ) -> Self
+    where Self: Sized,
+    {
+        let x: Self = AnnDataType::read(container).unwrap();
+        x.get_rows(idx)
+    }
 
-    fn to_view(&self, ridx: Vec<usize>, cidx: Vec<usize>) -> Box<dyn ContainerType>;
+    fn get_rows(&self, idx: Option<&[usize]>) -> Self where Self: Sized;
 }
 
-impl<T> AnnDataView for CsMat<T>
+impl<T> AnnDataGetRow for Array2<T>
 where
-    T: H5Type,
+    T: H5Type + Clone,
 {
-    fn read_partial(
+    fn get_rows(&self, idx: Option<&[usize]>) -> Self {
+        match idx {
+            None => self.clone(),
+            Some(i) => self.select(Axis(0), i),
+        }
+    }
+}
+
+impl<T> AnnDataGetRow for CsrMatrix<T>
+where
+    T: H5Type + Clone + Copy,
+{
+    fn get_rows(&self, idx: Option<&[usize]>) -> Self {
+        match idx {
+            None => self.clone(),
+            Some(i) => {
+                create_csr_from_rows(i.iter().map(|r| {
+                    let row = self.get_row(*r).unwrap();
+                    zip(row.col_indices(), row.values())
+                        .map(|(x, y)| (*x, *y)).collect()
+                }),
+                self.ncols()
+                )
+            }
+        }
+    }
+}
+
+pub trait AnnDataGetCol: AnnDataType {
+    fn read_columns(
         container: &Box<dyn ContainerType>,
-        ridx: Vec<usize>,
-        cidx: Vec<usize>,
-    ) -> Result<Self> where Self: Sized {
+        idx: Option<&[usize]>,
+    ) -> Self
+    where Self: Sized,
+    {
+        let x: Self = AnnDataType::read(container).unwrap();
+        x.get_columns(idx)
+    }
+
+    fn get_columns(&self, idx: Option<&[usize]>) -> Self where Self: Sized;
+}
+
+impl<T> AnnDataGetCol for Array2<T>
+where
+    T: H5Type + Clone,
+{
+    fn get_columns(&self, idx: Option<&[usize]>) -> Self {
+        match idx {
+            None => self.clone(),
+            Some(i) => self.select(Axis(1), i),
+        }
+    }
+}
+
+impl<T> AnnDataGetCol for CsrMatrix<T>
+where
+    T: H5Type + Clone,
+{
+    fn get_columns(&self, idx: Option<&[usize]>) -> Self {
         todo!()
     }
+}
 
-    fn to_view(&self, ridx: Vec<usize>, cidx: Vec<usize>) -> Box<dyn ContainerType> {
-
+pub trait AnnDataSubset: AnnDataGetRow + AnnDataGetCol {
+    fn read_partial(
+        container: &Box<dyn ContainerType>,
+        ridx: Option<&[usize]>,
+        cidx: Option<&[usize]>,
+    ) -> Self
+    where Self: Sized,
+    {
+        let x: Self = AnnDataGetRow::read_rows(container, ridx);
+        match cidx {
+            None => x,
+            _ => x.get_columns(cidx),
+        }
     }
+
+    fn subset(
+        &self,
+        ridx: Option<&[usize]>,
+        cidx: Option<&[usize]>
+    ) -> Self
+    where Self: Sized,
+    {
+        self.get_rows(ridx).get_columns(cidx)
+    }
+}
+
+impl<T> AnnDataSubset for CsrMatrix<T> where T: H5Type + Clone + Copy, {}
+impl<T> AnnDataSubset for Array2<T> where T: H5Type + Clone, {}
 
 pub fn downcast_anndata<T>(val: Box<dyn AnnDataType>) -> Box<T>
 where
@@ -311,3 +391,40 @@ where
         )
     }
 }
+
+/*
+pub fn cast_datatype(val: Box<dyn AnnDataType>) -> Box<dyn AnnDataSubset> {
+    let ptr = Box::into_raw(val);
+    let ty = unsafe { ptr.as_ref().unwrap().get_dtype() };
+    if true {
+        unsafe { Box::from_raw(ptr as *mut dyn AnnDataSubset) }
+    } else {
+        panic!(
+            "implementation error, cannot read {:?}",
+            ty,
+        )
+    }
+}
+*/
+
+fn create_csr_from_rows<I, T>(iter: I, num_col: usize) -> CsrMatrix<T>
+where
+    I: Iterator<Item = Vec<(usize, T)>>,
+    T: H5Type,
+{
+    let mut data: Vec<T> = Vec::new();
+    let mut indices: Vec<usize> = Vec::new();
+    let mut indptr: Vec<usize> = Vec::new();
+
+    let n = iter.fold(0, |r_idx, row| {
+        indptr.push(r_idx);
+        let new_idx = r_idx + row.len();
+        let (mut a, mut b) = row.into_iter().unzip();
+        indices.append(&mut a);
+        data.append(&mut b);
+        new_idx
+    });
+    indptr.push(n);
+    CsrMatrix::try_from_csr_data(indptr.len() - 1, num_col, indptr, indices, data).unwrap()
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -2,44 +2,125 @@ pub mod utils;
 pub mod anndata_trait;
 
 use anndata_trait::*;
-use crate::utils::{create_str_attr, read_str_attr, COMPRESSION};
+use crate::utils::{read_str_attr};
 
+use std::ops::Range;
 use std::boxed::Box;
-use std::sync::Arc;
-use std::any::Any;
-use hdf5::{File, H5Type, Result, Group, Dataset, types::VarLenUnicode};
-use hdf5::types::TypeDescriptor::*;
-use ndarray::{arr1, Array1, Array, Dimension};
-use itertools::Itertools;
-use std::marker::PhantomData;
-use sprs::CsMat;
+use hdf5::{File, Result, Group}; 
+use ndarray::{Array2};
+use nalgebra_sparse::csr::CsrMatrix;
+use std::collections::HashMap;
 
-pub struct Elem {
+pub struct Elem<T: ?Sized> {
     pub dtype: DataType,
-    element: Option<Box<dyn AnnDataType>>,
     container: Box<dyn ContainerType>,
+    element: Option<Box<T>>,
 }
 
-impl Elem {
-    pub fn new(container: Box<dyn ContainerType>) -> Result<Self> {
-        let dtype = container.get_encoding_type().expect(container.container_type());
-        Ok(Self { dtype, element: None, container })
-    }
+pub struct ElemView<T: ?Sized> {
+    obs_indices: Option<Vec<usize>>,
+    var_indices: Option<Vec<usize>>,
+    inner: Elem<T>,
+}
 
+// NOTE: this requires `element` is the last field, as trait object contains a vtable
+// at the end: https://docs.rs/vptr/latest/vptr/index.html.
+impl<T> AsRef<ElemView<T>> for ElemView<dyn AnnDataSubset>
+where
+    T: AnnDataType,
+{
+    fn as_ref(&self) -> &ElemView<T> {
+        if self.inner.dtype == T::dtype() {
+            unsafe { &*(self as *const ElemView<dyn AnnDataSubset> as *const ElemView<T>) }
+        } else {
+            panic!(
+                "implementation error, cannot convert {:?} to {:?}",
+                self.inner.dtype,
+                T::dtype(),
+            )
+        }
+    }
+}
+
+impl<T> AsRef<Elem<T>> for Elem<dyn AnnDataType>
+where
+    T: AnnDataType,
+{
+    fn as_ref(&self) -> &Elem<T> {
+        if self.dtype == T::dtype() {
+            unsafe { &*(self as *const Elem<dyn AnnDataType> as *const Elem<T>) }
+        } else {
+            panic!(
+                "implementation error, cannot convert {:?} to {:?}",
+                self.dtype,
+                T::dtype(),
+            )
+        }
+    }
+}
+
+impl Elem<dyn AnnDataType>
+{
+    pub fn csr_f32(&self) -> &Elem<CsrMatrix<f32>> { self.as_ref() }
+    pub fn csr_f64(&self) -> &Elem<CsrMatrix<f64>> { self.as_ref() }
+    pub fn csr_u32(&self) -> &Elem<CsrMatrix<u32>> { self.as_ref() }
+    pub fn csr_u64(&self) -> &Elem<CsrMatrix<u64>> { self.as_ref() }
+    pub fn arr2_f32(&self) -> &Elem<Array2<f32>> { self.as_ref() }
+    pub fn arr2_f64(&self) -> &Elem<Array2<f64>> { self.as_ref() }
+    pub fn arr2_u32(&self) -> &Elem<Array2<u32>> { self.as_ref() }
+    pub fn arr2_u64(&self) -> &Elem<Array2<u64>> { self.as_ref() }
+}
+
+impl ElemView<dyn AnnDataSubset>
+{
+    pub fn csr_f32(&self) -> &ElemView<CsrMatrix<f32>> { self.as_ref() }
+    pub fn csr_f64(&self) -> &ElemView<CsrMatrix<f64>> { self.as_ref() }
+    pub fn csr_u32(&self) -> &ElemView<CsrMatrix<u32>> { self.as_ref() }
+    pub fn csr_u64(&self) -> &ElemView<CsrMatrix<u64>> { self.as_ref() }
+    pub fn arr2_f32(&self) -> &ElemView<Array2<f32>> { self.as_ref() }
+    pub fn arr2_f64(&self) -> &ElemView<Array2<f64>> { self.as_ref() }
+    pub fn arr2_u32(&self) -> &ElemView<Array2<u32>> { self.as_ref() }
+    pub fn arr2_u64(&self) -> &ElemView<Array2<u64>> { self.as_ref() }
+}
+
+pub trait Subsettable {
+    fn subset(self, ridx: Option<&[usize]>, cidx: Option<&[usize]>) -> Result<Self> where Self: Sized;
+}
+
+// TODO: fix subsetting
+impl Subsettable for ElemView<dyn AnnDataSubset> {
+    fn subset(mut self, ridx: Option<&[usize]>, cidx: Option<&[usize]>) -> Result<Self>
+    where Self: Sized,
+    {
+        if let Some(obs_i) = ridx {
+            self.obs_indices = Some(obs_i.iter().map(|x| *x).collect());
+        }
+        if let Some(var_i) = cidx {
+            self.var_indices = Some(var_i.iter().map(|x| *x).collect());
+        }
+        if let Some(x) = self.inner.element {
+            self.inner.element = None;
+        }
+        Ok(self)
+    }
+}
+
+/*
+impl Elem<dyn AnnDataType> {
     pub fn read_elem(&self) -> Result<Box<dyn AnnDataType>> {
         let err = Err(hdf5::Error::Internal("not".to_string()));
         let container = &self.container;
         match self.dtype {
-            DataType::CsMatrix(Integer(_)) => {
-                let mat: CsMat<i64> = AnnDataType::read(container)?;
+            DataType::CsrMatrix(Integer(_)) => {
+                let mat: CsrMatrix<i64> = AnnDataType::read(container)?;
                 Ok(Box::new(mat))
             },
-            DataType::CsMatrix(Unsigned(_)) => {
-                let mat: CsMat<u64> = AnnDataType::read(container)?;
+            DataType::CsrMatrix(Unsigned(_)) => {
+                let mat: CsrMatrix<u64> = AnnDataType::read(container)?;
                 Ok(Box::new(mat))
             },
-            DataType::CsMatrix(Float(_)) => {
-                let mat: CsMat<f64> = AnnDataType::read(container)?;
+            DataType::CsrMatrix(Float(_)) => {
+                let mat: CsrMatrix<f64> = AnnDataType::read(container)?;
                 Ok(Box::new(mat))
             },
             _ => err?,
@@ -68,65 +149,104 @@ impl Elem {
         }
     }
 
-    pub fn csr_f32<'a>(&'a self) -> TypedElem<'a, CsMat<f32>> { self.typed() }
-    pub fn csr_f64<'a>(&'a self) -> TypedElem<'a, CsMat<f64>> { self.typed() }
-    pub fn csr_u32<'a>(&'a self) -> TypedElem<'a, CsMat<u32>> { self.typed() }
-    pub fn csr_u64<'a>(&'a self) -> TypedElem<'a, CsMat<u64>> { self.typed() }
 
 }
+*/
 
-pub struct TypedElem<'a, T> {
-    elem: &'a Elem,
-    phantom: PhantomData<T>,
-}
-
-impl<'a, T> TypedElem<'a, T>
+impl<T> Elem<T>
 where
     T: AnnDataType,
 {
-    pub fn read_elem(&self) -> Result<T> {
-        AnnDataType::read(&self.elem.container)
+    pub fn read_elem(&self) -> T { AnnDataType::read(&self.container).unwrap() }
+}
+
+impl<T> ElemView<T>
+where
+    T: AnnDataSubset,
+{
+    pub fn read_elem(&self) -> T {
+        AnnDataSubset::read_partial(
+            &self.inner.container,
+            self.obs_indices.as_ref().map(Vec::as_slice),
+            self.var_indices.as_ref().map(Vec::as_slice),
+        )
     }
 }
 
-//type Obsm = Box<dyn RowSlice<View=Box<dyn Any>> + AnnLoader<Data=Box<dyn Any>>>;
-//type Varm = Box<dyn VarmItem<View = Box<dyn Any>, Data = Box<dyn Any>>>;
-
-pub struct AnnData {
-    file: File,
-    pub x: Elem,
-    //var_names: Elem,
-    //obs_names: Elem,
-    obsm: Vec<Elem>,
-    varm: Vec<Elem>,
-}
-
-impl AnnData {
-    pub fn read(path: &str) -> Result<Self> {
-        let file = File::open(path)?;
-        let x = Elem::new(Box::new(file.group("X")?))?;
-        //let obs_names = read_dataframe_index(&file.group("obs")?)?;
-        //let var_names = read_dataframe_index(&file.group("var")?)?;
-        let mut obsm: Vec<Elem> = Vec::new();
-        //file.group("obsm")?.groups()?.into_iter().for_each(|x| {
-        //    obsm.push(Box::new(AnnDataType::read(&x).unwrap()));
-        //});
-        Ok(AnnData { file, x, obsm, varm: Vec::new() })
-    }
-
-    //pub fn x(&self) -> Box<csr::CsrMatrix<T>> {
-    //    todo!()
-    //}
-
-    //pub fn var_names(&mut self) -> &[String] { self.var_names.get().0.as_slice() }
-
-    //pub fn obs_names(&mut self) -> &[String] { self.obs_names.get().0.as_slice() }
-}
-
-pub fn read_dataframe_index(group: &Group) -> Result<Elem> {
+pub fn read_dataframe_index(group: &Group) -> Result<Elem<dyn AnnDataType>> {
     let index_name = read_str_attr(group, "_index")?;
     let elem = Box::new(group.dataset(&index_name)?);
     Elem::new(elem)
+}
+
+pub type AnnData = AnnDataBase<
+    ElemView<dyn AnnDataSubset>, ElemView<dyn AnnDataSubset>, ElemView<dyn AnnDataSubset>
+>;
+
+pub struct AnnDataBase<X, O, V> {
+    file: File,
+    pub x: X,
+    //var_names: Elem,
+    //obs_names: Elem,
+    pub obsm: HashMap<String, O>,
+    varm: Vec<V>,
+}
+
+pub trait Data {
+    fn new(container: Box<dyn ContainerType>) -> Result<Self> where Self: Sized;
+}
+
+impl Data for Elem<dyn AnnDataType> {
+    fn new(container: Box<dyn ContainerType>) -> Result<Self> {
+        let dtype = container.get_encoding_type().expect(container.container_type());
+        Ok(Self { dtype, element: None, container })
+    }
+}
+
+impl Data for ElemView<dyn AnnDataSubset> {
+    fn new(container: Box<dyn ContainerType>) -> Result<Self> {
+        let dtype = container.get_encoding_type().expect(container.container_type());
+        let inner = Elem { dtype, element: None, container };
+        Ok(Self { obs_indices: None, var_indices: None, inner })
+    }
+}
+
+impl<X, O, V> AnnDataBase<X, O, V> {
+    pub fn read(path: &str) -> Result<Self>
+    where
+        X: Data,
+        O: Data,
+        V: Data,
+    {
+        let file = File::open(path)?;
+        let x = Data::new(Box::new(file.group("X")?))?;
+        let get_name = |x: String| std::path::Path::new(&x).file_name()
+            .unwrap().to_str().unwrap().to_string();
+        //let obs_names = read_dataframe_index(&file.group("obs")?)?;
+        //let var_names = read_dataframe_index(&file.group("var")?)?;
+        let obsm =
+            file.group("obsm")?.groups()?.into_iter().map(|x|
+                (get_name(x.name()), Data::new(Box::new(x)).unwrap())
+            ).chain(file.group("obsm")?.datasets()?.into_iter().map(|x|
+                (get_name(x.name()), Data::new(Box::new(x)).unwrap())
+            )).collect();
+        Ok(Self { file, x, obsm, varm: Vec::new() })
+    }
+
+    pub fn obs_subset(mut self, idx: &[usize]) -> Self
+    where
+        X: Subsettable,
+        O: Subsettable,
+    {
+        AnnDataBase {
+            file: self.file,
+            x: self.x.subset(Some(idx), None).unwrap(),
+            obsm: self.obsm.drain()
+                .map(|(k, v)| (k, v.subset(Some(idx), None).unwrap()))
+                .collect(),
+            varm: self.varm,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -136,8 +256,16 @@ mod tests {
     #[test]
     fn test() {
         let adata: AnnData = AnnData::read("data.h5ad").unwrap();
-        let mat: CsMat<f32> = adata.x.typed().read_elem().unwrap();
-        println!("{:?}", mat.nnz());
+        println!(
+            "{:?}",
+            adata.obsm.get("X_spectral").unwrap().arr2_f64().read_elem(),
+        );
+
+        let adata_subset = adata.obs_subset(&[1,2,3]);
+        println!(
+            "{:?}",
+            adata_subset.obsm.get("X_spectral").unwrap().arr2_f64().read_elem(),
+        );
 
         //assert_eq!(beds, expected);
     }
